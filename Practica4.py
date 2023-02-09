242f92bb281ee712449fbcba18e16db642ff923f
Jose Estrada <jjjestradag@gmail.com>
2023-02-09 03:37:05 -0600

    08/02/23

--- Practica4.py
+++ Practica4.py
@@ -13,42 +13,38 @@ def zodiaco():
         dia = int(request.form.get('dia'))
         mes = int(request.form.get('mes'))
         ano = int(request.form.get('ano'))
-
         sex = request.form.get('sex')
-
-        P1 = int(request.form.get('P1'))
-        P2 = int(request.form.get('P2'))
-        P3 = int(request.form.get('P3'))
-        P4 = int(request.form.get('P4'))
-        P5 = int(request.form.get('P5'))
+        Pregunta1 = int(request.form.get('Pregunta1'))
+        Pregunta2 = int(request.form.get('Pregunta2'))
+        Pregunta3 = int(request.form.get('Pregunta3'))
+        Pregunta4 = int(request.form.get('Pregunta4'))
+        Pregunta5 = int(request.form.get('Pregunta5'))
 
         zodiac = ["Rata", "Buey", "Tigre", "Conejo", "Dragón", "Serpiente", "Caballo", "Cabra","Mono", "Gallo", "Perro", "Cerdo"]
         animal =  zodiac[(ano - 4) % 12]
 
         calificacion = 0
-        if P1 == 1:
+        if Pregunta1 == 1:
             calificacion += 2
-        if P2 == 1:
+        if Pregunta2 == 1:
             calificacion += 2
-        if P3 == 1:
+        if Pregunta3 == 1:
             calificacion += 2
-        if P4 == 1:
+        if Pregunta4 == 1:
             calificacion += 2
-        if P5 == 1:
+        if Pregunta5 == 1:
             calificacion += 2
 
         nombre = nombre +' '+ apaterno +' '+ amaterno
         
-        return render_template("Practica4-SignosZodiacal2.html",
+        return render_template("Practica4-2.html",
                                n = nombre, a = animal, c= calificacion, e = (2022-ano))
     else:
-        return render_template("Practica4-SignosZodiacal.html")
+        return render_template("Practica4.html")
 
 
 @app.route("/zodiaco2/",methods=['GET','POST'])
 def zodiaco2():
-    return render_template("Practica4-SignosZodiacal2.html",n = 'nombre', a = 'conejo', c= 8, e = (2022-2001))
+    return render_template("Practica4-2.html",n = 'nombre', a = 'toro', c= 8, e = (2022-2001))
 if __name__ == '__main__':
     app.run(debug=True)
-
-nom
